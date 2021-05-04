828c26e18c4be2e0095770f9adb9b41a4315db7a
sergiocardenasg <sergiocardenas@utexas.edu>
2021-05-03 21:09:02 -0500

    commit tree 3faf1b050c7ebf84be5e9849dae104c849cfc6be

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird, only: [:id, :name, :species]
   end
 end
